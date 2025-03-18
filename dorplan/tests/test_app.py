e5c33aa0d7b4e45d82acd4fa3eee5edce80a827f
Franco Peschiera <pchtsp@gmail.com>
2025-03-18 11:40:27 +0100

    working example

--- dorplan/tests/test_app.py
+++ dorplan/tests/test_app.py
@@ -4,10 +4,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-DATADIR = os.path.join(os.path.dirname(__file__), "data")
-ALMOST_KEYS = ["best_solution", "best_bound"]
-
-from data.graph_coloring import GraphColoring
+from .data.graph_coloring import GraphColoring
 from dorplan.app import DorPlan
 
 
